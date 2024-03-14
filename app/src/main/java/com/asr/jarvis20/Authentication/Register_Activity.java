fcff97f7bb992d04cb4ec10d82cfce43807374cd
naeem-2005 <naeemkhan7d@gmail.com>
2024-03-14 20:14:03 +0530

    Initial commit

---
+++ app/src/main/java/com/asr/jarvis20/Authentication/Register_Activity.java
@@ -0,0 +1,16 @@
+package com.asr.jarvis20.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asr.jarvis20.R;
+
+public class Register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
